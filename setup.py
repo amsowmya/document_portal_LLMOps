9084abb7d169ae31c74081c9bc609a90a492e629
amsowmya <soumya.pdit@gmail.com>
2025-08-05 22:03:56 +0530

    added files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_portal",
+    version="0.0.1",
+    author="Sowmya AM",
+    author_email="sowmya.anekonda@gmail.com",
+    packages=find_packages(),
+)
