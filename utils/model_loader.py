4a4fc1283f5b2c205263e6d7e93393b3731eca91
amsowmya <soumya.pdit@gmail.com>
2025-08-09 22:56:11 +0530

    added document analysis

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from utils.config_loader import load_config
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai.chat_models import ChatOpenAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_groq import ChatGroq
 from logger.custom_logger import CustomLogger
@@ -43,12 +44,74 @@ class ModelLoader:
         try:
             log.info("Loading embedding model...") 
             model_name = self.config['embedding_model']['model_name']
+            return GoogleGenerativeAIEmbeddings(model=model_name)
         except Exception as e:
             log.error("Error loading embedding model", error=str(e))
             raise
     
     def load_llm(self):
-        pass
+        """ 
+        Load and return the LLM model.
+        """
+        """Load LLM dynamically based on provider in config."""
+        
+        llm_block = self.config["llm"]
+        
+        log.info("Loading LLM...")
+        
+        provider_key = os.getenv("LLM_PROVIDER", "groq") # Default groq 
+        
+        if provider_key not in llm_block:
+            log.error("LLM provider not found in config", provider_key=provider_key)
+            raise ValueError(f"Provider '{provider_key}' not found in config")
+        
+        llm_config = llm_block[provider_key]
+        provider = llm_config.get("provider")
+        model_name = llm_config.get("model_name")
+        temperature = llm_config.get("temperature", 0.2)
+        max_tokens = llm_config.get("max_output_tokens", 2048)
+        
+        log.info("Loading LLM", provider=provider, model=model_name, temperature=temperature, max_tokens=max_tokens)
+        
+        if provider == "google":
+            llm = ChatGoogleGenerativeAI(
+                model=model_name,
+                temperature=temperature,
+                max_output_tokens=max_tokens
+            )
+            return llm 
+        
+        elif provider == "groq":
+            llm = ChatGroq(
+                model=model_name,
+                api_key=self.api_keys['GROQ_API_KEY'],
+                temperature=temperature
+            )
+            return llm
+        
+        elif provider == "openai":
+            return ChatOpenAI(
+                model=model_name,
+                api_key=self.api_keys["OPENAI_API_KEY"],
+                temperature=temperature,
+                max_tokens=max_tokens
+            )
+            
+        else:
+            log.error("Unsupported LLM Provider", provider=provider)
+            raise ValueError(f"Unsupported LLM provider: {provider}")
     
     
-obj = ModelLoader()
+
+if __name__ == "__main__":
+    obj = ModelLoader()
+    
+    llm = obj.load_llm()
+    print(f"LLM Loaded... {llm}")
+    print(llm.invoke("hi").content)
+    
+    embeddings = obj.load_embeddings()
+    print(f"Embedding model loaded: {embeddings}")
+    
+    result = embeddings.embed_query("Hi, How are you?")
+    print(f"Embeddings: {result}")
